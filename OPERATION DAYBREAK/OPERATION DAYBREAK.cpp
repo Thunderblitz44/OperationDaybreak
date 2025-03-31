cf2e985d991d96e054b1c9ff7a4c9a65d7ed665b
Shallowcat <joshR364@hotmail.com>
2025-03-30 21:20:26 -0400

    comment out new stuff

--- OPERATION DAYBREAK/OPERATION DAYBREAK.cpp
+++ OPERATION DAYBREAK/OPERATION DAYBREAK.cpp
@@ -10,54 +10,54 @@ using namespace std;
 
 int main()
 {
-#include <memory>
-#include "Character.h"
-	namespace Game {//part of the base code from https://learn.ontariotechu.ca/courses/30946/assignments/184835
-		int Character::count = 0;
-		Character::Character() {
-			cout << "Character constructor called. " << "Characters active: " << ++Character::count << "\n";
-		}
-		Character::~Character() { cout << "Character destructor called. " << "Characters remaining: " << --Character::count << "\n";
-		}
-		Player::Player() {
-			cout << "Player constructor called\n";
-		}
-		void Player::speak() const {
-			cout << "I need to find an exit\n";// == change this for character's speech == can be changed to a string variable, to allow for
-		}
-		Player::~Player() {
-			cout << "Player destructor called. \n";
-		}
-		Zombie::Zombie() {
-			cout << "Zombie constructor called\n";
-		}
-		void Zombie::speak() const {
-			cout << "GRRRRRRR\n";// == change this for character's speech
-		}
-		Zombie::~Zombie() {
-			cout << "Zombie destructor called \n";
-		}
-		Survivor::Survivor() {
-			cout << "Survivor constructor called\n";
-		}
-		void Survivor::speak() const {
-			cout << "I've been saved!\n";// == change this for character's speech
-		}
-		Survivor::~Survivor() {
-			cout << "Survivor destructor called \n";
-		}
-
-		unique_ptr<Player> p(new Player());// Found using prompt in google "unique_ptr in c++" https://www.geeksforgeeks.org/unique_ptr-in-cpp/
-		unique_ptr<Zombie> z(new Zombie());
-		unique_ptr<Survivor> s(new Survivor());
-	}
-	using namespace Game;
-
-	cout << endl;
-	p->speak();// use these to speak (this uses inheritence and polymorphism)
-	z->speak();
-	s->speak();
-	cout << endl;
+//#include <memory>
+//#include "Character.h"
+//	namespace Game {//part of the base code from https://learn.ontariotechu.ca/courses/30946/assignments/184835
+//		int Character::count = 0;
+//		Character::Character() {
+//			cout << "Character constructor called. " << "Characters active: " << ++Character::count << "\n";
+//		}
+//		Character::~Character() { cout << "Character destructor called. " << "Characters remaining: " << --Character::count << "\n";
+//		}
+//		Player::Player() {
+//			cout << "Player constructor called\n";
+//		}
+//		void Player::speak() const {
+//			cout << "I need to find an exit\n";// == change this for character's speech == can be changed to a string variable, to allow for
+//		}
+//		Player::~Player() {
+//			cout << "Player destructor called. \n";
+//		}
+//		Zombie::Zombie() {
+//			cout << "Zombie constructor called\n";
+//		}
+//		void Zombie::speak() const {
+//			cout << "GRRRRRRR\n";// == change this for character's speech
+//		}
+//		Zombie::~Zombie() {
+//			cout << "Zombie destructor called \n";
+//		}
+//		Survivor::Survivor() {
+//			cout << "Survivor constructor called\n";
+//		}
+//		void Survivor::speak() const {
+//			cout << "I've been saved!\n";// == change this for character's speech
+//		}
+//		Survivor::~Survivor() {
+//			cout << "Survivor destructor called \n";
+//		}
+//
+//		unique_ptr<Player> p(new Player());// Found using prompt in google "unique_ptr in c++" https://www.geeksforgeeks.org/unique_ptr-in-cpp/
+//		unique_ptr<Zombie> z(new Zombie());
+//		unique_ptr<Survivor> s(new Survivor());
+//	}
+//	using namespace Game;
+//
+//	cout << endl;
+//	p->speak();// use these to speak (this uses inheritence and polymorphism)
+//	z->speak();
+//	s->speak();
+//	cout << endl;
 
 
 	srand(time(0)); // Seed the random generator
