a8e5f255a36e6565434e39139091cf8ec12be48f
Not-Bean <alexander.bean@ontariotechu.net>
2025-03-30 21:52:41 -0400

    fixed

--- OPERATION DAYBREAK/OPERATION DAYBREAK.cpp
+++ OPERATION DAYBREAK/OPERATION DAYBREAK.cpp
@@ -5,27 +5,27 @@
 #include <string>
 #include <vector>
 #include "Header.h"
+#include "Character.h"
 
 using namespace std;
-
-int main()
-{
+using namespace Game;
 #include <memory>
-#include "Character.h"
-	//namespace hgshks{//part of the base code from https://learn.ontariotechu.ca/courses/30946/assignments/184835
+
+namespace Game {//part of the base code from https://learn.ontariotechu.ca/courses/30946/assignments/184835
 	int Character::count = 0;
 	Character::Character() {
 		cout << "Character constructor called. " << "Characters active: " << ++Character::count << "\n";
 	}
-	Character::~Character() { cout << "Character destructor called. " << "Characters remaining: " << --Character::count << "\n";
+	Character::~Character() {
+		cout << "Character destructor called. " << "Characters remaining: " << --Character::count << "\n";
 	}
-	Player::Player1() {
+	Player1::Player1() {
 		cout << "Player constructor called\n";
 	}
 	void Player1::speak() const {
 		cout << "I need to find an exit\n";// == change this for character's speech == can be changed to a string variable, to allow for
 	}
-	Player::~Player1() {
+	Player1::~Player1() {
 		cout << "Player destructor called. \n";
 	}
 	Zombie::Zombie() {
@@ -47,17 +47,20 @@ int main()
 		cout << "Survivor destructor called \n";
 	}
 
+
+}
+
+int main()
+{
 	unique_ptr<Player1> p(new Player1());// Found using prompt in google "unique_ptr in c++" https://www.geeksforgeeks.org/unique_ptr-in-cpp/
 	unique_ptr<Zombie> z(new Zombie());
 	unique_ptr<Survivor> s(new Survivor());
-	}
-	using namespace Game;
 
-	cout << endl;
+	std::cout << endl;
 	p->speak();// use these to speak (this uses inheritence and polymorphism)
 	z->speak();
 	s->speak();
-	cout << endl;
+	std::cout << endl;
 
 
 	srand(time(0)); // Seed the random generator
@@ -72,13 +75,13 @@ int main()
 		player.turn();
 		if(player.pos[0] == map.goalPos[0] && player.pos[1] == map.goalPos[1])
 		{
-			cout << "You have found your target\n";
+			std::cout << "You have found your target\n";
 
-			cout << "Target: Great you made it lets get out of here\n";
+			std::cout << "Target: Great you made it lets get out of here\n";
 
-			cout << "You have completed your mission\n";
+			std::cout << "You have completed your mission\n";
 
-			cout << "Congratulations!\n";
+			std::cout << "Congratulations!\n";
 
 			play = false;
 		}
